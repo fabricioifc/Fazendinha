f8828f86a58266499924789fbecc56e0b1f83ee0
Angelita Zanella <gelirettore@gmail.com>
2022-06-08 13:51:03 -0300

    pasta com scripts para criacao das tabelas

--- object.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE [object] ( 
-	id INTEGER PRIMARY KEY AUTOINCREMENT,
-	timestamp DATETIME  NOT NULL,
-	instance INTEGER  NOT NULL,
-	resource varchar(8) NOT NULL,
-	value REAL NOT NULL,
-	status INTEGER NOT NULL
-); 
