0f1fa2f0a3758f00261ce54e8c6f528c215fe406
Fabricio Bizotto <fabricio.bizotto@ifc.edu.br>
2022-06-08 15:19:19 -0300

    tarefas criadas

--- README.md
+++ README.md
@@ -8,12 +8,22 @@ Angelina's Farm
 > João Danielewicz\
 > Outro Membro\
 
-ToDo
+## ToDo List
 
- - Criar uma tela de login para .....
-    - [ ] todo
-    - [ ] todo
-    - [ ] todo
-    - [ ] todo
-    - [ ] todo
+- [ ] Criar uma tela de `Ambientes` para que seja possível cadastrar cada um dos ambientes
+   - [x] name (TEXT) - required
+   - [x] status INTEGER (1 para TRUE ou 0 para FALSE) - required (`checkbox`)
+> Exemplos: [1 - Horta, 2 - Plantação 2]
 
+ - [ ] Criar uma tela de `Instâncias` para que seja possível cadastrar cada uma das caixinhas
+   - [x] name (TEXT) - required
+   - [x] instance_number (INTEGER) - required
+   - [x] status INTEGER (1 para TRUE ou 0 para FALSE) - required (`checkbox`)
+> Exemplos: [Transdutor 1, Transdutor 2 ...]
+
+- [ ] Criar uma tela de `Recursos` para que seja possível cadastrar cada um dos sensores
+   - [x] name (TEXT) - required
+   - [x] resource_number (INTEGER) - required
+   - [x] vlini (REAL) - required
+   - [x] vlfim (REAL) - required (deve ser maior que o inicial)
+> Exemplos: [(3304, 'Umidade do Ar', 0, 100), (3304, 'Temperatura', 0, 40)]
--- tabelas/object.sql
+++ tabelas/object.sql
@@ -1,8 +1,40 @@
-CREATE TABLE [object] ( 
+CREATE TABLE leituras ( 
 	id INTEGER PRIMARY KEY AUTOINCREMENT,
 	timestamp DATETIME  NOT NULL,
 	instance INTEGER  NOT NULL,
 	resource varchar(8) NOT NULL,
 	value REAL NOT NULL,
-	status INTEGER NOT NULL
-); 
+	status INTEGER NOT NULL,
+	FOREIGN KEY(instance) REFERENCES instance(instance_number)
+);
+
+-- [1 - Plantação 1, 2 - Plantação 2, 3 - Plantação 3]
+-- [Horta 1, Horta 2]
+CREATE TABLE instances (
+	id INTEGER PRIMARY KEY AUTOINCREMENT,
+	name TEXT NOT NULL,
+	instance_number INTEGER not null,
+	status INTEGER NOT NULL DEFAULT 1
+);
+
+CREATE INDEX instance_idx ON instance (instance_number);
+
+CREATE TABLE resources (
+	id INTEGER PRIMARY KEY AUTOINCREMENT,
+	name TEXT NOT NULL,
+	resource_number INTEGER 
+		CHECK( resource_number IN (3303, 3304) ) not null,
+	status INTEGER NOT NULL DEFAULT 1,
+	normal INTEGER NOT NULL DEFAULT 1,
+);
+
+
+CREATE INDEX resource_idx ON instances (instance_number);
+
+-- Python
+-- [(3304, 'Umidade do Ar'), (3304, 'Temperatura')]
+
+-- 1 para 1 -> escolhe onde vai a FK
+-- 1 para N -> FK vai no N
+
+-- N Instâncias para N Recursos (Implementar depois)
