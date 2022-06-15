9bc7b95690c8ff47647200f848dc622aba54fb3a
João Danielewicz <joao.wicz@gmail.com>
2022-06-15 13:53:38 -0300

    commit

--- dbProvisorio/app.py
+++ dbProvisorio/app.py
@@ -6,8 +6,6 @@ from flask import Flask, flash, render_template, redirect, request, url_for
 
 app = Flask(__name__)
 
-app = Flask(__name__)
-
 def get_db_connection():
     conn = sqlite3.connect('database.db')
     conn.row_factory = sqlite3.Row
