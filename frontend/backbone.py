04378e5f1df6585aa4ce28384dfd492fc61e34f5
matheuswogt37 <matheusnotas07@gmail.com>
2022-06-15 14:05:41 -0300

    inserido cadastro de recurso não funcional

--- frontend/backbone.py
+++ frontend/backbone.py
@@ -1,6 +1,12 @@
-from flask import Flask, redirect, url_for, render_template, request
+import sqlite3
+from flask import Flask, flash, redirect, url_for, render_template, request
 
 app = Flask(__name__)
+
+def get_db_connection():
+    conn = sqlite3.connect('database.db')
+    conn.row_factory = sqlite3.Row
+    return conn
 
 
 @app.route('/base')
@@ -59,12 +65,26 @@ def cadRecurso():
     if valorInicial>valorFinal:
         return render_template("cadastroRecursos.html", aviso="O valor final não pode ser maior que o inicial. Tente novamente.")
     else:
-        nomeRecurso = "nomeRecurso"
-        recursoNumero = "recursoNumero"
-        valorInicial = request.form["vlIniAmb"]
-        valorFinal = request.form["vlFinAmb"]
-        return redirect(url_for("/verdados"))
 
+        name = request.form['nomeRecurso']
+        resource_number = request.form['recursoNumero']
+
+        if not name:
+            flash('É obrigatório inserir um nome')
+        elif not resource_number:
+            flash('É obrigatório definir um número de recurso')
+        elif not valorInicial:
+            flash('É obrigatório definir um valor inicial')
+        elif not valorFinal:
+            flash('É obrigatório definir um valor final')
+        else:
+            conn = get_db_connection()
+            conn.execute('INSERT INTO resources (name, resource_number, vlini, vlfim) VALUES (?, ?, ?, ?)', (name, resource_number, valorInicial, valorFinal))
+            conn.commit()
+            conn.close()
+            return redirect(url_for('home'))
+
+       
 @app.route('/cadastro/recursos', methods=["GET"])
 def getRecurso():
     return render_template("cadastroRecursos.html")
