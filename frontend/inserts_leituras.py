c8629707fedfd7487896704b32d343249d72565d
matheuswogt37 <matheusnotas07@gmail.com>
2022-07-26 17:12:20 -0300

    ajeitado leituras e estudos sobre graficos

--- frontend/inserts_leituras.py
+++ frontend/inserts_leituras.py
@@ -2,9 +2,8 @@ import sqlite3
 conn = sqlite3.connect("bancoDados.db")
 cur=conn.cursor()
 
-cur.execute("""
-INSERT INTO leituras VALUES ('2020-06-21 00:00:33', '3', '3316', '3.97')
+cur.execute(""" 
+INSERT INTO readings (hour_reading, id_instance_FK, number_resource_FK, value) VALUES ('2020-06-21 00:06:58', '3', '3303', '21.8') 
 """)
-
 conn.commit()
 conn.close()
